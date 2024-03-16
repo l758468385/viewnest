b446fe009da2960a513ed9c6ec773fea319f5c98
lirk <lirk@goktech.cn>
2024-03-16 14:38:32 +0800

    feat:华明到家

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>华铭到家</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+h1 {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
